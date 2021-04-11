34a154fbc6901bb515a8f3e75c021ca2a01ed3cd
Crystal K Young <crystal.young@codeimmersive.com>
2021-04-11 15:14:39 -0400

    finished

---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { NavLink, Link } from "react-router-dom"
+
+import "./Navbar.css"
+
+function Navbar() {
+    return (     
+        <header className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-body border-bottom shadow-sm">
+        <p className="h5 my-0 me-md-auto fw-normal">
+            <Link to="/">
+            <i className="fas fa-user-circle"></i>
+            {/* Login App */}
+            </Link>
+        </p>
+        <nav className="my-2 my-md-0 me-md-3">
+            <NavLink
+                exact
+                className="p-2"
+                activeClassName="active-class-style"
+                to="/">Home</NavLink>
+        </nav>
+        <NavLink
+            className="btn btn-outline-success"
+            activeStyle={{ color: "magenta" }}
+            to="/sign-up">Sign Up</NavLink>
+
+        <NavLink
+            className="btn btn-outline-primary"
+            activeStyle={{ color: "magenta" }}
+            to="/login">Login</NavLink>
+    </header>
+    
+    )
+}
+
+
+export default Navbar
